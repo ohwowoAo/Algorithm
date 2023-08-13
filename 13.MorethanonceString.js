2a1e4de84c37687b1567f6b95530bdd2f26a19d9
ohwowoAo <sbjdnm@gmail.com>
2023-08-13 22:55:30 +0900

    Feat : 두번이상 사용한 최초 문자 찾기

---
+++ 13.MorethanonceString.js
@@ -0,0 +1,11 @@
+//두번이상 사용한 최초 문자
+function solution(s) {
+  cnt = {};
+  for (let x of s) {
+    cnt[x] = (cnt[x] || 0) + 1;
+    if (cnt[x] === 2) return x;
+  }
+  // console.log(cnt);
+  return -1;
+}
+console.log(solution("abccbaaxz"));
