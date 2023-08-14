3327c9720a2c5fe9c5174909c91d13b4e7b69010
ohwowoAo <sbjdnm@gmail.com>
2023-08-14 23:25:52 +0900

    Add : 두번째 풀이 방법 추가

--- 15.SameFrequency.js
+++ 15.SameFrequency.js
@@ -2,23 +2,34 @@
 function solution(nums) {
   let answer = [];
   let sH = new Map();
+  //두번째방법
+  let maxV = 0;
 
   for (let x of nums) {
-    sH.set(x, (sH.get(x) || 0) + 1);
-    // console.log(sH);
+    sH[x] = (sH[x] || 0) + 1;
+    maxV = Math.max(maxV, sH[x]);
+  }
+  for (let x of Object.keys(sH)) {
+    answer.push(maxV - sH[x]);
   }
 
-  let MaxV = 0;
-  for (let [key, val] of sH) {
-    console.log(key);
-    if (MaxV < val) {
-      MaxV = val;
-      // console.log(MaxV);
-      answer.push(MaxV - val);
-    } else {
-      answer.push(MaxV - val);
-    }
-  }
+  //첫번째방법
+  // for (let x of nums) {
+  //   sH.set(x, (sH.get(x) || 0) + 1);
+  //   // console.log(sH);
+  // }
+
+  // let MaxV = 0;
+  // for (let [key, val] of sH) {
+  //   console.log(key);
+  //   if (MaxV < val) {
+  //     MaxV = val;
+  //     // console.log(MaxV);
+  //     answer.push(MaxV - val);
+  //   } else {
+  //     answer.push(MaxV - val);
+  //   }
+  // }
   return answer;
 }
 console.log(solution("aaabc"));
